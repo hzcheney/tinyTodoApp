820d5c802c9b4f918cd89f3f8808f0b325ed0c42
陈翰 <chenhan@a.lan>
2020-03-07 23:20:19 +0800

    practice dom manipulation

--- app.js
+++ app.js
@@ -2,38 +2,69 @@
 
 // console.log(document.getElementById('task-title'));
 
-// // Get things from the element
+// Get things from the element
 // console.log(document.getElementById('task-title').id);
 // console.log(document.getElementById('task-title').className);
 
 // const taskTitle = document.getElementById('task-title');
 
-// // Change styling
-// taskTitle.style.background = '#333';
+// Change styling
+// taskTitle.style.background = 'green';
 // taskTitle.style.color = '#fff';
 // taskTitle.style.padding = '5px';
-// // taskTitle.style.display = 'none';
+// taskTitle.style.display = 'none';
 
-// // Change content
+// Change content
 // taskTitle.textContent = 'Task List';
 // taskTitle.innerText = 'My Tasks';
 // taskTitle.innerHTML = '<span style="color:red">Task List</span>';
 
 // document.querySelector()
 
-console.log(document.querySelector('#task-title'));
-console.log(document.querySelector('.card-title'));
-console.log(document.querySelector('h5'));
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
 
-document.querySelector('li').style.color = 'red';
-document.querySelector('ul li').style.color = 'blue';
+// document.querySelector('li').style.color = 'red';
+// document.querySelector('ul li').style.color = 'blue';
 
-document.querySelector('li:last-child').style.color = 'red';
-document.querySelector('li:nth-child(3)').style.color = 'yellow';
-document.querySelector('li:nth-child(4)').textContent = 'Hello World';
-document.querySelector('li:nth-child(odd)').style.background = '#ccc';
-document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
+// document.querySelector('li:last-child').style.color = 'red';
+// document.querySelector('li:nth-child(3)').style.color = 'yellow';
+// document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+// document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+// document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
 
+// document.getElementsByTagName
+// let lis = document.getElementsByTagName('li');
+
+// Convert HTML Collection into array
+// lis = Array.from(lis);
+
+// lis.reverse();
+
+// lis.forEach(function(li, index) {
+// console.log(li.className);
+// li.textContent = `${index}: Hello`;
+//     li.textContent = `${index}: hello`;
+// });
+
+// document.querySelectorAll
+const items = document.querySelectorAll('ul.collection li.collection-item');
+
+items.forEach(function(item, index){
+    item.textContent = `${index}: Hello`;
+});
+
+const liOdd = document.querySelectorAll('li:nth-child(odd');
+const liEven = document.querySelectorAll('li:nth-child(even');
+
+liOdd.forEach(function(li,index){
+    li.style.background = '#ccc';    
+});
+
+for(let i = 0; i < liEven.length; i++){
+    liEven[i].style.background = '#f4f4f4';
+  }
 
 
 
